b9e54196d8c15040e8de4ceebefd07709ce0fffa
Alexandre Batista <89105629+BatistaAlexandre@users.noreply.github.com>
2022-03-30 22:37:42 -0300

    index

--- index.js.js
+++ index.js.js
@@ -1,19 +1,23 @@
-function likeOrDislike(arr) {
+function likeOrDislike(arr) { // declarada a função: likeOrDislike
     let result  = 'Nothing';
-  
-      for (let i = 0; i < arr.length; i += 1) {
-          if (arr[i] === 'Like'){
-              if (result === 'Like') {
+                
+      for (let i = 0; i < arr.length; i += 1) {  
+      // o iten [i] que o usuario procura seriam os botões de Like e Dislike
+
+          if (arr[i] === 'Like'){ // se o usuario estiver procurando o [i]=Like então resultará em [Like]
+
+              if (result === 'Like') {  //se o usuario desmarcar ou clicar 2x o botão de Like então resultará em [Nothing]                 
                   result = 'Nothing';
               } 
-						else {result = 'Like';}
+					else {result = 'Like';} // caso o usuario não clique novamente no botão Like, então resultará em [Like]
           }
               
-          if (arr[i] === 'Dislike') {
-              if (result === 'Dislike') {
+          if (arr[i] === 'Dislike'){ // se o usuario estiver procurando o [i] Dislike então resultará em [Dislike]
+
+              if (result === 'Dislike'){ //se o usuario desmarcar ou clicar 2x o botão de Dislike então resultará em [Nothing]
                 result = 'Nothing';
               } 
-						else {result = 'Dislike'; }
+						else {result = 'Dislike'; } // Caso o usuario não clique novamente no botão Dislike, então resultará em [Dislike]
           }
       }
       return result;
